d0bf4c421b757247f4b1b3a330a7cd02843f08ce
Andres-Parra2610 <andres_alejandro_parra@hotmail.com>
2025-11-19 09:56:21 -0800

    feat: Introduce `Base` entity for common fields and refactor `User` entity to extend it, removing redundant ID and timestamp fields, consolidating name fields into `fullName`, and making `email` and `password` mandatory.

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,17 +1,12 @@
+import { Base } from 'src/shared/entities/base-entity';
 import { City } from 'src/shared/entities/city.entity';
 import { State } from 'src/shared/entities/state.entity';
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, Column, ManyToOne, JoinColumn } from 'typeorm';
 
 @Entity({ name: 'users' })
-export class User {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
-  @Column({ name: 'name', type: 'text' })
-  name: string;
-
-  @Column({ name: 'lastname', type: 'text', nullable: true })
-  lastname: string;
+export class User extends Base {
+  @Column({ name: 'full_name', type: 'text' })
+  fullName: string;
 
   @Column({ name: 'identity_document', type: 'text', unique: true })
   identityDocument: string;
@@ -19,46 +14,20 @@ export class User {
   @Column({ name: 'type_document', type: 'text', enum: ['V', 'J', 'E', 'G'] })
   typeDocument: string;
 
+  @Column({ name: 'contact_number', type: 'text', nullable: true })
+  contactNumber: string;
+
+  @Column({ name: 'email', type: 'text' })
+  email: string;
+
+  @Column({ name: 'password', type: 'text' })
+  password: string;
+
   @Column({ name: 'state_id', type: 'uuid', nullable: true })
   stateId: string;
 
   @Column({ name: 'city_id', type: 'uuid', nullable: true })
   cityId: string;
-
-
-  @Column({ name: 'physical_address', type: 'text', nullable: true })
-  physicalAddress: string;
-
-  @Column({ name: 'contact_number', type: 'text', nullable: true })
-  contactNumber: string;
-
-  @Column({ name: 'email', type: 'text', nullable: true })
-  email: string;
-
-  @Column({ name: 'password', type: 'text', nullable: true })
-  password: string;
-
-  @Column({
-    name: 'user_type',
-    type: 'text',
-    enum: ['vendedor', 'cliente', 'admin']
-  })
-  userType: string;
-
-  @Column({ name: 'client_type_id', type: 'bigint', nullable: true })
-  clientTypeId: number;
-
-
-  @CreateDateColumn({ name: 'created_at' })
-  createdAt: Date;
-
-  @UpdateDateColumn({ name: 'updated_at' })
-  updatedAt: Date;
-
-  @DeleteDateColumn({ name: 'deleted_at' })
-  deletedAt: Date;
-
-
 
   /* Relations */
   @ManyToOne(() => State, { eager: true, nullable: true })
