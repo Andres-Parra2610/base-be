44df6ed16380182937a4ba100ecbbfdeea384acc
Andres-Parra2610 <andres_alejandro_parra@hotmail.com>
2026-01-29 11:14:21 -0400

    hash user password

--- src/modules/user/application/use-cases/create-user.usecase.ts
+++ src/modules/user/application/use-cases/create-user.usecase.ts
@@ -2,18 +2,22 @@ import { IUserRepository } from '../../domain/ports/user-repository.port';
 import { UserModel } from '../../domain/models/user.model';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { generateUuidV4 } from '@/src/utils/uuid/generate-uuid';
+import { hashPassword } from '@/src/utils/hashes/hash-password';
 
 export class CreateUserUseCase {
   constructor(private readonly userRepository: IUserRepository) {}
 
   async execute(createUserDto: CreateUserDto): Promise<UserModel> {
+    UserModel.validatePassword(createUserDto.password || '');
+
     const user = new UserModel({
       id: generateUuidV4(),
       fullName: createUserDto.fullName,
       email: createUserDto.email,
-      password: createUserDto.password,
+      password: hashPassword(createUserDto.password || ''),
       isStaff: createUserDto.isStaff,
     });
+
     return this.userRepository.create(user);
   }
 }
--- src/modules/user/domain/models/user.model.ts
+++ src/modules/user/domain/models/user.model.ts
@@ -1,4 +1,5 @@
 import { BaseModel, BaseModelParams } from '@/src/shared/domain/models/base.model';
+import { validateRegex } from '@/src/utils/regex/validatorHelper.regex';
 
 interface UserModelParams extends BaseModelParams {
   fullName: string;
@@ -15,9 +16,22 @@ export class UserModel extends BaseModel {
 
   constructor(params: UserModelParams) {
     super(params);
+
+    this.validateEmail(params.email);
+
     this.fullName = params.fullName;
     this.email = params.email;
     this.password = params.password;
     this.isStaff = params.isStaff;
+  }
+
+  private validateEmail(email: string) {
+    const isValid = validateRegex(email, 'email');
+    if (isValid) throw new Error(isValid);
+  }
+
+  static validatePassword(password: string) {
+    const isValid = password.length >= 6;
+    if (!isValid) throw new Error('La contraseña debe tener al menos 6 caracteres');
   }
 }
