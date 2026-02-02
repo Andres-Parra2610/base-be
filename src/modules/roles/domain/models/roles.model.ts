ed5b3640ebb8387f466c1d8b6d19139cf313b8e2
Andres-Parra2610 <andres_alejandro_parra@hotmail.com>
2026-02-02 16:13:18 -0400

    validate user permission

--- src/modules/roles/domain/models/roles.model.ts
+++ src/modules/roles/domain/models/roles.model.ts
@@ -1,25 +1,62 @@
 import { BaseModel, BaseModelParams } from '@/shared/domain/models/base.model';
-import { ContextType } from '../types/roles.types';
+import {
+  AppPermissions,
+  ContextType,
+  PermissionAction,
+  PermissionResource,
+} from '../types/roles.types';
+import { DomainError } from '@/src/utils/errors/domain.error';
 
 export interface RolesModelParams extends BaseModelParams {
   id: string;
   context_type: ContextType;
   context_id?: string;
   name: string;
-  permissions: string;
+  permissions: AppPermissions;
 }
 
 export class RolesModel extends BaseModel<RolesModelParams> {
   context_type: ContextType;
   context_id?: string;
   name: string;
-  permissions: string;
+  permissions: AppPermissions;
 
   constructor(params: RolesModelParams) {
     super(params);
+    this.validatePermissions(params.permissions);
     this.context_type = params.context_type;
     this.context_id = params.context_id;
     this.name = params.name;
     this.permissions = params.permissions;
+  }
+
+  private validatePermissions(permissions: AppPermissions) {
+    const resources = Object.keys(permissions);
+
+    const validResources = Object.values(PermissionResource) as string[];
+    const validActions = Object.values(PermissionAction) as string[];
+
+    for (const resource of resources) {
+      if (!validResources.includes(resource)) {
+        throw new DomainError(`El módulo de permisos '${resource}' no es válido.`);
+      }
+
+      const actions = Object.keys(permissions[resource as PermissionResource] || {});
+      for (const action of actions) {
+        if (!validActions.includes(action)) {
+          throw new DomainError(`La acción '${action}' en el módulo '${resource}' no es válida.`);
+        }
+      }
+    }
+  }
+
+  public can(resource: PermissionResource, action: PermissionAction): boolean {
+    const resourcePermissions = this.permissions[resource];
+
+    if (!resourcePermissions || !resourcePermissions[action]) {
+      return false;
+    }
+
+    return true;
   }
 }
--- src/modules/roles/domain/types/roles.types.ts
+++ src/modules/roles/domain/types/roles.types.ts
@@ -3,3 +3,20 @@ export enum ContextType {
   association = 'application',
   producer = 'producer',
 }
+
+export enum PermissionResource {
+  USER = 'user',
+  FARM = 'farm',
+  ANIMAL = 'animal',
+}
+
+export enum PermissionAction {
+  CREATE = 'create',
+  READ = 'read',
+  UPDATE = 'update',
+  DELETE = 'delete',
+  EXPORT = 'export',
+}
+
+export type PermissionRules = Partial<Record<PermissionAction, boolean>>;
+export type AppPermissions = Partial<Record<PermissionResource, PermissionRules>>;
