3e2219f0a35c89cc3e4df033bcca1e115f828245
Andres-Parra2610 <andres_alejandro_parra@hotmail.com>
2026-02-03 11:25:22 -0400

    create migrations

--- src/modules/user/infrastucture/persistence/entities/user-role.entity.ts
+++ src/modules/user/infrastucture/persistence/entities/user-role.entity.ts
@@ -1,6 +1,6 @@
 import { Entity, Column, ManyToOne, JoinColumn, Unique } from 'typeorm';
 import { BaseEntity } from '@/src/shared/infrastructure/persistent/typeorm/entity/base-entity';
-import { UserEntity } from './user.entity';
+import type { UserEntity } from './user.entity';
 import { RolesEntity } from '@/src/modules/roles/infrastucture/persistence/entities/roles.entity';
 
 @Entity('user_roles')
@@ -12,7 +12,7 @@ export class UserRoleEntity extends BaseEntity {
   @Column({ name: 'role_id', type: 'uuid' })
   roleId: string;
 
-  @ManyToOne(() => UserEntity)
+  @ManyToOne('UserEntity')
   @JoinColumn({ name: 'user_id', foreignKeyConstraintName: 'FK_user_roles_user_id' })
   user: UserEntity;
 
