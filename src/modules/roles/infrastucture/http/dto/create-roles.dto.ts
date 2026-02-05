6025498ad28368bdbbb567c5ad51e8d8b508d630
Andres-Parra2610 <andres_alejandro_parra@hotmail.com>
2026-02-05 15:34:02 -0400

    add lowercase to rol

--- src/modules/roles/infrastucture/http/dto/create-roles.dto.ts
+++ src/modules/roles/infrastucture/http/dto/create-roles.dto.ts
@@ -1,9 +1,11 @@
 import { IsString, IsNotEmpty, IsBoolean, IsObject, IsOptional, MaxLength } from 'class-validator';
 import { AppPermissions } from '../../../domain/types/roles.types';
+import { Transform } from 'class-transformer';
 
 export class CreateRolesDto {
   @IsString()
   @IsNotEmpty()
+  @Transform(({ value }: { value: string }) => value.trim().toLowerCase())
   name: string;
 
   @IsObject()
